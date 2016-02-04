d23b0845d54821554e4587f0028173855c427b7a
CptSurfinBird <jacobgittings@gmail.com>
2016-02-04 11:51:31 -0500

    updated app.js with anonymous function

--- scripts/app.js
+++ scripts/app.js
@@ -6,6 +6,7 @@
 	console.log("App Started...");
 	
 	// declared a named function <- use for Tom's class
+	/*
 	function replaceFirstParagraph() {
 		console.log("inside replaceFirstParagraph function");
 		var firstParagraph;
@@ -13,8 +14,19 @@
 		firstParagraph = document.getElementById("firstParagraph");
 		
 		firstParagraph.innerHTML = "My New Paragraph Data";
-	
 	}
+	*/
+	
+	// declare an anonymous function with named alias
+	var replaceFirstParagraph = function() {
+		console.log("inside replaceFirstParagraph function");
+		var firstParagraph;
+		
+		firstParagraph = document.getElementById("firstParagraph");
+		
+		firstParagraph.innerHTML = "My New Paragraph Data";		
+	};
+	
 	
 	// call the replaceFirstParagraph function
 	replaceFirstParagraph();
